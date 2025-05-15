08b613e1cc626b894a5c0675b924890c91046c03
Kyaw Lin <kokyawlin@gmail.com>
2025-05-15 11:11:50 +0800

    Add files via upload

--- BackEnd/tutornet/src/main/java/com/project/tutornet/business/TutorService.java
+++ BackEnd/tutornet/src/main/java/com/project/tutornet/business/TutorService.java
@@ -1,5 +1,12 @@
 package com.project.tutornet.business;
+import org.springframework.stereotype.Service;
 
+import jakarta.transaction.Transactional;
+import lombok.extern.slf4j.Slf4j;
+
+@Service
+@Slf4j
+@Transactional
 public class TutorService {
 
 }
