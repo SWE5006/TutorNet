27fb20ce5784917286330ea06a1834185dbcd7c8
Kyaw Lin <kokyawlin@gmail.com>
2025-05-27 13:43:36 +0800

    change table name of user to users
    change table name of user to users

--- BackEnd/tutornet/src/main/java/com/project/tutornet/entity/UserInfoEntity.java
+++ BackEnd/tutornet/src/main/java/com/project/tutornet/entity/UserInfoEntity.java
@@ -12,6 +12,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -22,7 +23,7 @@ import lombok.Setter;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-
+@Table(name="users")
 public class UserInfoEntity {
 
 	@Id
