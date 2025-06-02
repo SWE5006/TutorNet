d68d23c546a50e20785d67f5f1666d53c0b10444
Kyaw Lin <kokyawlin@gmail.com>
2025-06-02 13:01:07 +0800

    change some service in frontend
    change some service in frontend

--- FrontEnd/src/services/student.service.ts
+++ FrontEnd/src/services/student.service.ts
@@ -1,6 +1,22 @@
-import { createApi } from "@reduxjs/toolkit/query/react";
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { commonHeader, fetchBaseQueryAuthMiddleware } from "../utils";
 import { RootState } from "../state/store";
+export const studentReducerName = "studentApi";
 
-export const userReducerName = "tutorApi";
+export const studentApi = createApi({
+  reducerPath: studentReducerName,
+  baseQuery: fetchBaseQuery({
+    baseUrl: process.env.GATSBY_BACKEND_API_URL + "/api/student",
+    credentials: "include",
+    prepareHeaders: commonHeader,
+  }),
+  tagTypes: ['Student'],
+  endpoints: (builder) => ({
+    getStudentById: builder.query({
+      query: (studentId: string) => `/${studentId}`,
+      providesTags: (result, error, studentId) => [{ type: 'Student', id: studentId }],
+    }),
+  }),
+});
 
+export const { useGetStudentByIdQuery } = studentApi;
--- FrontEnd/src/services/tutor.service.ts
+++ FrontEnd/src/services/tutor.service.ts
@@ -1,6 +1,23 @@
-import { createApi } from "@reduxjs/toolkit/query/react";
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { commonHeader, fetchBaseQueryAuthMiddleware } from "../utils";
 import { RootState } from "../state/store";
 
-export const userReducerName = "tutorApi";
+export const tutorReducerName = "tutorApi";
 
+export const tutorApi = createApi({
+  reducerPath: tutorReducerName,
+  baseQuery: fetchBaseQuery({
+    baseUrl: process.env.GATSBY_BACKEND_API_URL + "/api/tutor",
+    credentials: "include",
+    prepareHeaders: commonHeader,
+  }),
+  tagTypes: ['Tutor'],
+  endpoints: (builder) => ({
+    getTutorById: builder.query({
+      query: (tutorId: string) => `/${tutorId}`,
+      providesTags: (result, error, tutorId) => [{ type: 'Tutor', id: tutorId }],
+    }),
+  }),
+});
+
+export const { useGetTutorByIdQuery } = tutorApi;
