594cc96f417d4ff9f42d348ff713a87a77c0fbfe
Kyaw Lin <kokyawlin@gmail.com>
2025-06-10 20:36:57 +0800

    Add some images for signup form
    Add some images for signup form and login

--- FrontEnd/src/pages/login.tsx
+++ FrontEnd/src/pages/login.tsx
@@ -13,6 +13,7 @@ import { useLoginMutation } from "../services/auth.service";
 import { selectAuthSlice } from "../state/auth/slice";
 import { useSelector } from "react-redux";
 import { navigate } from "gatsby";
+import { Box as MuiBox } from "@mui/material"; // Rename to avoid confusion
 
 const authUrl = process.env.GATSBY_BACKEND_API_URL;
 const Login = () => {
@@ -37,94 +38,143 @@ const Login = () => {
   }, [isLoggedIn]);
 
   return isLoggedIn ? null : (
-    <Container component="main" maxWidth="xs">
-      <Paper
-        elevation={6}
+    <Container maxWidth="lg" sx={{ height: "100vh", display: "flex", alignItems: "center" }}>
+      <MuiBox
         sx={{
-          mt: 8,
-          p: 4,
           display: "flex",
-          flexDirection: "column",
+          justifyContent: "space-between",
           alignItems: "center",
-          borderRadius: 2,
+          width: "100%",
+          bgcolor: "background.paper",
+          borderRadius: 3,
+          boxShadow: 3,
+          overflow: "hidden",
         }}
       >
-        <Box component="form" noValidate sx={{ mt: 1, width: "100%" }}>
-          <TextField
-            margin="normal"
-            required
-            fullWidth
-            id="email"
-            label="Email Address"
-            name="email"
-            autoComplete="email"
-            autoFocus
-            value={emailAddress}
-            onChange={(e) => setEmailAddress(e.target.value)}
-            sx={{ mb: 2 }}
-          />
-          <TextField
-            margin="normal"
-            required
-            fullWidth
-            name="password"
-            label="Password"
-            type="password"
-            id="password"
-            autoComplete="current-password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            sx={{ mb: 3 }}
-          />
-          <Button
-            fullWidth
-            variant="contained"
-            onClick={onLogin}
-            sx={{ mt: 1, mb: 2, py: 1.5 }}
-          >
-            Login
-          </Button>
-          {result.isError && (
-            <Typography color="error" sx={{ mt: 1 }}>
-              Login failed. Please try again.
-            </Typography>
-          )}
-          <Divider sx={{ my: 3 }}>OR</Divider>
-          <Button
-            fullWidth
-            variant="outlined"
-            onClick={onLoginWithGoogle}
-            startIcon={
-              <img
-                src="/google.svg"
-                alt="Google Logo"
-                style={{ width: 20, marginRight: 10 }}
-              />
-            }
-            sx={{ mb: 2, py: 1.5 }}
-          >
-            Login with Google
-          </Button>
-          <Box
-            sx={{
-              display: "flex",
-              justifyContent: "space-between",
+        {/* Left side - Image */}
+        <MuiBox
+          sx={{
+            flex: 1,
+            height: "600px",
+            display: { xs: "none", md: "block" }, // Hide on mobile
+          }}
+        >
+          <img
+            src="/images/loginpic.jpg"
+            alt="Login"
+            style={{
               width: "100%",
-              mt: 2,
+              height: "100%",
+              objectFit: "cover",
+            }}
+          />
+        </MuiBox>
+
+        {/* Right side - Login Form */}
+        <MuiBox
+          sx={{
+            flex: 1,
+            p: 4,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Paper
+            elevation={0}
+            sx={{
+              p: 4,
+              width: "100%",
+              maxWidth: 400,
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              borderRadius: 2,
             }}
           >
-            <Typography>Don't have an account?</Typography>
-            <Typography><Link href="/signupstudent" variant="body2">
-              Sign Up as Student
-            </Link></Typography>
-            
-            <Typography><Link href="/signuptutor" variant="body2">
-              Sign Up as Tutor
-            </Link></Typography>
-          </Box>
-        </Box>
-      </Paper>
+            <Box component="form" noValidate sx={{ mt: 1, width: "100%" }}>
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+                autoFocus
+                value={emailAddress}
+                onChange={(e) => setEmailAddress(e.target.value)}
+                sx={{ mb: 2 }}
+              />
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                sx={{ mb: 3 }}
+              />
+              <Button
+                fullWidth
+                variant="contained"
+                onClick={onLogin}
+                sx={{ mt: 1, mb: 2, py: 1.5 }}
+              >
+                Login
+              </Button>
+              {result.isError && (
+                <Typography color="error" sx={{ mt: 1 }}>
+                  Login failed. Please try again.
+                </Typography>
+              )}
+              <Divider sx={{ my: 3 }}>OR</Divider>
+              <Button
+                fullWidth
+                variant="outlined"
+                onClick={onLoginWithGoogle}
+                startIcon={
+                  <img
+                    src="/google.svg"
+                    alt="Google Logo"
+                    style={{ width: 20, marginRight: 10 }}
+                  />
+                }
+                sx={{ mb: 2, py: 1.5 }}
+              >
+                Login with Google
+              </Button>
+              <Box
+                sx={{
+                  display: "flex",
+                  justifyContent: "space-between",
+                  width: "100%",
+                  mt: 2,
+                }}
+              >
+                <Typography>Don't have an account?</Typography>
+                <Typography>
+                  <Link href="/signupstudent" variant="body2">
+                    Sign Up as Student
+                  </Link>
+                </Typography>
+
+                <Typography>
+                  <Link href="/signuptutor" variant="body2">
+                    Sign Up as Tutor
+                  </Link>
+                </Typography>
+              </Box>
+            </Box>
+          </Paper>
+        </MuiBox>
+      </MuiBox>
     </Container>
   );
 };
+
 export default Login;
--- FrontEnd/src/pages/signupstudent.tsx
+++ FrontEnd/src/pages/signupstudent.tsx
@@ -15,6 +15,7 @@ import {
   FormHelperText,
 } from "@mui/material";
 import { useSignUpStudentMutation } from "../services/auth.service";
+import { Box as MuiBox } from "@mui/material";
 
 export default function SignUpPage() {
   const [userName, setUserName] = useState("");
@@ -104,198 +105,226 @@ export default function SignUpPage() {
   }, [result]);
 
   return (
-    <Container component="main" maxWidth="xs">
-      <Paper
-        elevation={6}
+    <Container 
+      maxWidth="lg" 
+      sx={{ 
+        height: "100vh", 
+        display: "flex", 
+        alignItems: "center",
+        pl: 3,
+      }}
+    >
+      <MuiBox
         sx={{
-          mt: 8,
-          p: 4,
           display: "flex",
-          flexDirection: "column",
+          justifyContent: "flex-end",
           alignItems: "center",
-          borderRadius: 2,
+          width: "100%",
+          height: "650px",
+          bgcolor: "background.paper",
+          borderRadius: 3,
+          boxShadow: 3,
+          overflow: "hidden",
+          backgroundImage: `url('/images/signuptutor.jpg')`,
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+          backgroundRepeat: 'no-repeat',
         }}
       >
-        <Typography variant="h4" textAlign="center">
-          Sign Up
-        </Typography>
-        <Box
-          component="form"
-          onSubmit={handleSignUpStudent}
-          noValidate
-          sx={{ mt: 1, width: "100%" }}
+        <Paper
+          elevation={6}
+          sx={{
+            p: 4,
+            width: "100%",
+            maxWidth: 400,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            borderRadius: 2,
+          }}
         >
-          <TextField
-            required
-            fullWidth
-            label="User Name"
-            variant="outlined"
-            value={userName}
-            error={!!nameError}
-            helperText={nameError}
-            onChange={(e) => {
-              setUserName(e.target.value);
-              if (e.target.value.length >= 1) {
-                setNameError(false);
-              } else {
-                setNameError("User name is required.");
-              }
-            }}
-            margin="normal"
-          />
-
-          <TextField
-            fullWidth
-            required
-            label="Email Address"
-            variant="outlined"
-            type="email"
-            value={userEmail}
-            inputProps={{
-              type: "email",
-            }}
-            error={!!emailError}
-            helperText={emailError}
-            onChange={(e) => {
-              setEmailAddress(e.target.value);
-              if (e.target.validity.valid) {
-                setEmailError(false);
-              } else {
-                setEmailError("Please provide valid email address.");
-              }
-            }}
-            margin="normal"
-          />
-
-          <TextField
-            required
-            fullWidth
-            label="Password"
-            variant="outlined"
-            type="password"
-            value={userPassword}
-            error={!!passwordError}
-            helperText={passwordError}
-            onChange={(e) => {
-              setPassword(e.target.value);
-              if (
-                !/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/.test(
-                  e.target.value
-                )
-              ) {
-                setPasswordError(
-                  "Invalid password, your password must be: Minimum eight characters, at least one uppercase letter, one lowercase letter, one number and one special character"
-                );
-              } else {
-                setPasswordError(false);
-              }
-            }}
-            margin="normal"
-          />
-
-          <TextField
-            required
-            fullWidth
-            label="Contact Number"
-            variant="outlined"
-            type="text"
-            value={userMobileNo}
-            error={!!mobileNoError}
-            helperText={mobileNoError}
-            onChange={(e) => {
-              setMobile(e.target.value);
-              if (
-                e.target.value.length == 8 &&
-                !isNaN(Number(e.target.value))
-              ) {
-                setMobileError(false);
-              } else {
-                setMobileError("Please provide valid singapore mobile number.");
-              }
-            }}
-            margin="normal"
-          />
-
-          <TextField
-            required
-            fullWidth
-            label="Age"
-            variant="outlined"
-            type="number"
-            value={userAge}
-            error={!!ageError}
-            helperText={ageError}
-            onChange={(e) => {
-              setUserAge(e.target.value);
-              const age = parseInt(e.target.value);
-              if (age >= 5 && age <= 100) {
-                setAgeError(false);
-              } else {
-                setAgeError("Please provide a valid age between 5 and 100.");
-              }
-            }}
-            margin="normal"
-            inputProps={{
-              min: 5,
-              max: 100
-            }}
-          />
-
-       
-            <InputLabel id="class-level-label">Class Level</InputLabel>
-            <Select
+          <Typography variant="h4" textAlign="center">
+            Sign Up as Student
+          </Typography>
+          
+          <Box
+            component="form"
+            onSubmit={handleSignUpStudent}
+            noValidate
+            sx={{ mt: 1, width: "100%" }}
+          >
+            <TextField
+              required
               fullWidth
-              labelId="class-level-label"
-              value={userClassLevel}
-              label="Class Level"
+              label="User Name"
+              variant="outlined"
+              value={userName}
+              error={!!nameError}
+              helperText={nameError}
               onChange={(e) => {
-                setUserClassLevel(e.target.value);
-                if (e.target.value) {
-                  setClassLevelError(false);
+                setUserName(e.target.value);
+                if (e.target.value.length >= 1) {
+                  setNameError(false);
                 } else {
-                  setClassLevelError("Please select a class level.");
+                  setNameError("User name is required.");
                 }
               }}
-             sx={{ marginBottom: 2 }} 
-            >
-              {classLevels.map((level) => (
-                <MenuItem key={level} value={level}>
-                  {level}
-                </MenuItem>
-              ))}
-            </Select>
-            {classLevelError && (
-              <FormHelperText>{classLevelError}</FormHelperText>
-            )}
-   
-          <Button
-            variant="contained"
-            color="primary"
-            onClick={handleSignUpStudent}
-            disabled={result.isLoading}
+              margin="normal"
+            />
+
+            <TextField
+              fullWidth
+              required
+              label="Email Address"
+              variant="outlined"
+              type="email"
+              value={userEmail}
+              inputProps={{
+                type: "email",
+              }}
+              error={!!emailError}
+              helperText={emailError}
+              onChange={(e) => {
+                setEmailAddress(e.target.value);
+                if (e.target.validity.valid) {
+                  setEmailError(false);
+                } else {
+                  setEmailError("Please provide valid email address.");
+                }
+              }}
+              margin="normal"
+            />
+
+            <TextField
+              required
+              fullWidth
+              label="Password"
+              variant="outlined"
+              type="password"
+              value={userPassword}
+              error={!!passwordError}
+              helperText={passwordError}
+              onChange={(e) => {
+                setPassword(e.target.value);
+                if (
+                  !/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/.test(
+                    e.target.value
+                  )
+                ) {
+                  setPasswordError(
+                    "Invalid password, your password must be: Minimum eight characters, at least one uppercase letter, one lowercase letter, one number and one special character"
+                  );
+                } else {
+                  setPasswordError(false);
+                }
+              }}
+              margin="normal"
+            />
+
+            <TextField
+              required
+              fullWidth
+              label="Contact Number"
+              variant="outlined"
+              type="text"
+              value={userMobileNo}
+              error={!!mobileNoError}
+              helperText={mobileNoError}
+              onChange={(e) => {
+                setMobile(e.target.value);
+                if (
+                  e.target.value.length == 8 &&
+                  !isNaN(Number(e.target.value))
+                ) {
+                  setMobileError(false);
+                } else {
+                  setMobileError("Please provide valid singapore mobile number.");
+                }
+              }}
+              margin="normal"
+            />
+
+            <TextField
+              required
+              fullWidth
+              label="Age"
+              variant="outlined"
+              type="number"
+              value={userAge}
+              error={!!ageError}
+              helperText={ageError}
+              onChange={(e) => {
+                setUserAge(e.target.value);
+                const age = parseInt(e.target.value);
+                if (age >= 5 && age <= 100) {
+                  setAgeError(false);
+                } else {
+                  setAgeError("Please provide a valid age between 5 and 100.");
+                }
+              }}
+              margin="normal"
+              inputProps={{
+                min: 5,
+                max: 100
+              }}
+            />
+
+     
+          <InputLabel id="class-level-label">Class Level</InputLabel>
+          <Select
             fullWidth
-          >
-            Sign Up AS Student
-          </Button>
-          {result.isError && (
-            <Typography color="error">
-              Sign up failed. Please try again.
-            </Typography>
-          )}
-          <Box
-            sx={{
-              display: "flex",
-              justifyContent: "center",
-              width: "100%",
-              mt: 2,
+            labelId="class-level-label"
+            value={userClassLevel}
+            label="Class Level"
+            onChange={(e) => {
+              setUserClassLevel(e.target.value);
+              if (e.target.value) {
+                setClassLevelError(false);
+              } else {
+                setClassLevelError("Please select a class level.");
+              }
             }}
+           sx={{ marginBottom: 2 }} 
           >
-            <Link href="/login" variant="body2">
-              {"Already have an account? Login"}
-            </Link>
+            {classLevels.map((level) => (
+              <MenuItem key={level} value={level}>
+                {level}
+              </MenuItem>
+            ))}
+          </Select>
+          {classLevelError && (
+            <FormHelperText>{classLevelError}</FormHelperText>
+          )}
+ 
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={handleSignUpStudent}
+              disabled={result.isLoading}
+              fullWidth
+            >
+              Sign Up AS Student
+            </Button>
+            {result.isError && (
+              <Typography color="error">
+                Sign up failed. Please try again.
+              </Typography>
+            )}
+            <Box
+              sx={{
+                display: "flex",
+                justifyContent: "center",
+                width: "100%",
+                mt: 2,
+              }}
+            >
+              <Link href="/login" variant="body2">
+                {"Already have an account? Login"}
+              </Link>
+            </Box>
           </Box>
-        </Box>
-      </Paper>
+        </Paper>
+      </MuiBox>
     </Container>
   );
 }
